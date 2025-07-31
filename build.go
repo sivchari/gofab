91df3587fb72fb9c5d349588bef06d29d8c4c7ed
sivchari <shibuuuu5@gmail.com>
2025-07-31 15:27:15 +0900

    fix lint
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- build.go
+++ build.go
@@ -1,25 +1,24 @@
 package gofab
 
-// Build creates a single instance with automatic field population based on struct tags
+// Build creates a single instance with automatic field population based on struct tags.
 func Build[T any](customizers ...func(*T)) T {
 	var result T
-	
-	// Auto-populate using struct tags
+
 	autoPopulateFromTags(&result)
-	
-	// Apply any custom overrides
+
 	for _, customizer := range customizers {
 		customizer(&result)
 	}
-	
+
 	return result
 }
 
-// BuildList creates multiple instances with automatic field population
+// BuildList creates multiple instances with automatic field population.
 func BuildList[T any](count int, customizers ...func(*T)) []T {
 	result := make([]T, count)
 	for i := range result {
 		result[i] = Build[T](customizers...)
 	}
+
 	return result
 }
